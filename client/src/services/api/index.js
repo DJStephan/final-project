58ed2ba428afa2a8192e56e27860e3038ce2a960
Seong <kirakim96@hotmail.com>
2018-12-05 17:43:02 -0600

    Edited api

--- client/src/services/api/index.js
+++ client/src/services/api/index.js
@@ -23,46 +23,34 @@ const axiosPatch = (url, params) => {
 
 export const uploadFile = (data) =>
   axiosPost(`upload/file`,{params: {file: data}})
-    .then(response => console.log(response))
 
 export const uploadFolder = (data) =>
   axiosPost(`upload/folder`,{params: data})
-    .then(response => console.log(response))
 
 
 export const downloadFile = (fileId) =>
   axiosGet('download/file',{fileId: fileId})
-    .then(response => console.log(response))
 export const downloadFolder = (folderId) =>
   axiosGet('download/folder',{folderId: folderId})
-    .then(response => console.log(response))
 
 
 export const deleteFile = (fileId) =>
   axiosDelete('edit/delete/file',{fileId: fileId})
-    .then(response => console.log(response))
 export const deleteFolder = (folderId) =>
   axiosDelete('edit/delete/folder',{folderId: folderId})
-    .then(response => console.log(response))
 
 export const createFolder = (folderId, folderName) =>
   axiosPost('edit/create',{params: {folderId: folderId, folderName: folderName}})
-    .then(response => console.log(response))
-    .catch(err => console.log('error'))
 
 export const moveFile = (fileId, locationFolderId) =>
   axiosPatch('edit/move/file',{fileId: fileId, locationFolderId: locationFolderId})
-    .then(response => console.log(response))
 export const moveFolder = (folderId, locationFolderId) =>
   axiosPatch('edit/move/folder',{folderId: folderId, locationFolderId: locationFolderId})
-    .then(response => console.log(response))
 
 export const renameFile = (fileId, newName) =>
   axiosPatch('edit/rename/file', {fileId: fileId, newName: newName})
-    .then(response => console.log(response))
 export const renameFolder = (folderId, newName) =>
   axiosPatch('edit/rename/folder', {folderId: folderId, newName: newName})
-    .then(response => console.log(response))
 
 export const view = (id) =>
   axiosGet(`view/${id}`)
