4a49e86e63c0c47759fe18ba49c3b8497979b64b
Forest Hill <forestwhill@gmail.com>
2018-12-05 11:35:08 -0600

    Updated File component. Incomplete

--- client/src/components/File/index.js
+++ client/src/components/File/index.js
@@ -1,29 +1,45 @@
 import React from 'react'
+import connect from 'react-redux/es/connect/connect'
 import PropTypes from 'prop-types'
 import {
-  IconButton,
   ListItem,
   ListItemIcon,
-  ListItemSecondaryAction,
   ListItemText
 } from '@material-ui/core'
-import { MdDelete, MdInsertDriveFile } from 'react-icons/md'
+import { MdInsertDriveFile } from 'react-icons/md'
 
-const File = ({ last, name }) =>
-  <ListItem button divider={last}>
+import { updateSelectedId } from '../../ducks/filetree.duck'
+
+const File = ({ id, last, name, selectedId, updateSelectedId }) =>
+  <ListItem
+    button
+    divider={last}
+    selected={id === selectedId}
+    onClick={() => updateSelectedId(id)}
+  >
     <ListItemIcon>
       <MdInsertDriveFile />
     </ListItemIcon>
     <ListItemText primary={name} />
-    <ListItemSecondaryAction>
-      <IconButton>
-        <MdDelete />
-      </IconButton>
-    </ListItemSecondaryAction>
   </ListItem>
 
 File.propTypes = {
-  name: PropTypes.string.isRequired
+  // functions
+  updateSelectedId: PropTypes.func.isRequired,
+  // required vars
+  name: PropTypes.string.isRequired,
+  // optional vars
+  id: PropTypes.number,
+  last: PropTypes.bool,
+  selectedId: PropTypes.number
 }
 
-export default File
+const mapStateToProps = state => ({
+  selectedId: state.selectedId
+})
+
+const mapDispatchToProps = dispatch => ({
+  updateSelectedId: id => dispatch(updateSelectedId(id))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(File)
