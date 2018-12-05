c0976703463ce055e1b264b2f2fcf27ac631814f
Austin Sauer <inventorguy2235@hotmail.com>
2018-12-05 09:46:46 -0600

    View Mapper in progress

---
+++ server/src/main/java/driverstorage/server/mapper/ViewMapper.java
@@ -0,0 +1,33 @@
+package driverstorage.server.mapper;
+
+import java.util.List;
+
+import javax.swing.text.View;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import driverstorage.server.dto.FolderDto;
+import driverstorage.server.dto.StructureFolderDto;
+import driverstorage.server.entity.Folder;
+
+@Mapper(componentModel = "spring")
+public interface ViewMapper {
+	@Mappings({ @Mapping(target = "folderName", source = "entitiy.folderName"),
+			@Mapping(target = "files", source = "entity.files"),
+			@Mapping(target = "folders", source = "entity.folders") })
+
+	FolderDto entityToDto(Folder entity);
+
+	List<FolderDto> entitysToDtos(List<Folder> entity);
+
+	@Mappings({ @Mapping(target = "view", source = "dto.view"),
+		@Mapping(target = "files", source = "dto.files"),
+		@Mapping(target = "folders", source = "dto.folders") })
+
+	View dtoToEntity(StructureFolderDto dto);
+
+	List<View> dtosToEntitys(List<StructureFolderDto> dto);
+
+}
