a559494ed21ea2fe626e23cba54f6d50511b6f65
Forest Hill <forestwhill@gmail.com>
2018-12-05 12:06:40 -0600

    Changed file structure

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,17 +1,21 @@
 import Button from './Button'
 import File from './File'
+import Folder from './Folder'
 import Header from './Header'
 import Move from './Move'
 import NewFolder from './NewFolder'
+import Sidebar from './Sidebar'
 import Title from './Title'
-import Upload from './Upload'
+// import Upload from './Upload'
 
 export {
   Button,
   File,
+  Folder,
   Header,
   Move,
   NewFolder,
+  Sidebar,
   Title,
-  Upload
+  // Upload
 }
--- client/src/containers/index.js
+++ client/src/containers/index.js
@@ -1,9 +1,7 @@
 import Browser from './Browser'
-import Folder from './Folder'
 import Page from './Page'
 
 export {
   Browser,
-  Folder,
   Page
 }
