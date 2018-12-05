73ef48425854824cd1c06ebb85b9cd7fbab989e0
Forest Hill <forestwhill@gmail.com>
2018-12-05 12:09:23 -0600

    Updated Page container. Incomplete.

--- client/src/containers/Page/index.js
+++ client/src/containers/Page/index.js
@@ -3,6 +3,7 @@ import { CssBaseline, MuiThemeProvider } from '@material-ui/core'
 import { createMuiTheme } from '@material-ui/core/styles'
 
 import { Browser } from '..'
+// import { Sidebar } from '../../components'
 
 const theme = createMuiTheme({
   typography: {
@@ -16,6 +17,10 @@ class Page extends Component {
       <Fragment>
         <CssBaseline />
         <MuiThemeProvider theme={theme}>
+          {/* <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Sidebar />
+            <Browser />
+          </div> */}
           <Browser />
         </MuiThemeProvider>
       </Fragment>
