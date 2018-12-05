19e8d245fc4faac515c95b75a2e16e4e14bc2b33
David Stephan <djstephan21@gmail.com>
2018-12-05 09:02:50 -0600

    updated file structure

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,17 +1,17 @@
 import Button from './Button'
 import File from './File'
-import Folder from './Folder'
 import Header from './Header'
 import Move from './Move'
 import NewFolder from './NewFolder'
 import Title from './Title'
+import Upload from './Upload'
 
 export {
   Button,
   File,
-  Folder,
   Header,
   Move,
   NewFolder,
-  Title
+  Title,
+  Upload
 }
