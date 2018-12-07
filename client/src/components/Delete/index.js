0cd39ae76bf4a77aebd9dbc40c81b61b67d681c2
Kalkazar <joecurrystodder@gmail.com>
2018-12-07 12:32:34 -0600

    minor edits to delete button. still needs ducks to work

--- client/src/components/Delete/index.js
+++ client/src/components/Delete/index.js
@@ -7,89 +7,84 @@ import { ListItem, Button } from '@material-ui/core'
 import Dialog from '@material-ui/core/Dialog'
 import Slide from '@material-ui/core/Slide'
 
-import { createFolder, deleteFile, deleteFolder } from '../../services/api'
+import { deleteFile, deleteFolder } from '../../services/api'
 
 function Transition(props) {
-  return <Slide direction="up" {...props} />;
+    return <Slide direction="up" {...props} />;
 }
 
 class Delete extends Component {
-  constructor() {
-    super()
-    this.state = {
-      open: false,
-      name: '[fileName/folderName]',
-      type: '[selected element]'
+    constructor() {
+        super()
+        this.state = {
+            open: false,
+            name: '[fileName/folderName]',
+            type: '[selected element]'
+        }
+        this.textInput = React.createRef();
     }
-    this.textInput = React.createRef();
-  }
 
-  state = {
-    open: false,
-    name: '[fileName/folderName]',
-    type: '[selected element]'
-  }
-
-  handleOpen = () => {
-    if(!this.state.open) {
-      this.setState({ 
-        open: true 
-      });
+    state = {
+        open: false,
+        name: '[fileName/folderName]',
+        type: '[selected element]'
     }
-  }
 
-  handleClose = () => {
-    this.setState({
-      open: false 
-    })
-  }
+    handleOpen = () => {
+        if(!this.state.open) {
+            this.setState({ 
+                open: true 
+            });
+        }
+    }
 
-  createNewFolder = () => {
-    let id = 1
-    createFolder(id, this.state.name)
-      .then(response => console.log(response))
-      .catch(err => console.log(err));
-    this.handleClose()
-    this.setState({name: 'New Folder'})
-  }
+    handleClose = () => {
+        this.setState({
+            open: false 
+        })
+    }
 
-  deleteSelected = () => {
-      //
-  }
+    deleteSelected = () => {
+        console.log(this.state)
+        // NOT DONE! Final version must do the following:
+        //   - Determine whether it's a folder or file
+        //   - Edit state.name and state.type accordingly (for the dialogue box)
+        //   - Call the appropriate function on the file or folder.
+    }
 
-  render() {
-    return (
-      <ListItem button onClick = {this.handleOpen}>
-        {this.props.children}
-        <Dialog
-          open={this.state.open}
-          onClose={this.handleClose}
-          TransitionComponent={Transition}
-          keepMounted
-          aria-labelledby="alert-dialog-slide-title"
-          aria-describedby="alert-dialog-slide-description"
-          scroll="paper"
-        >
-        {/* Should we center everything? */}
-          <DialogTitle>Delete {this.state.type}</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Are you sure you want to delete {this.state.name}
-            </DialogContentText>
-          </DialogContent>
-          {/* Maybe we should center or re-order these buttons... */}
-          <DialogActions>
-            <Button onClick={this.createNewFolder} color="primary">
-              Yes
-            </Button>
-            <Button onClick={this.handleClose} color="primary">
-              No
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </ListItem>
-    );
-  }
+    render() {
+        return (
+            <ListItem button onClick = {this.handleOpen}>
+                {this.props.children}
+                <Dialog
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                    TransitionComponent={Transition}
+                    keepMounted
+                    aria-labelledby="alert-dialog-slide-title"
+                    aria-describedby="alert-dialog-slide-description"
+                    scroll="paper"
+                >
+                    {/* Should we center everything? It might look better... */}
+                    <DialogTitle>Delete {this.state.type}</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Are you sure you want to delete {this.state.name}?
+                        </DialogContentText>
+                    </DialogContent>
+                    {/* Maybe we should re-order these buttons as well... */}
+                    <DialogActions>
+                        <Button onClick={this.deleteSelected} color="primary">
+                            Yes
+                        </Button>
+                        <Button onClick={this.handleClose} color="primary">
+                            No
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </ListItem>
+        );
+    }
 }
 
 export default Delete
