93ecb67549707d246a6e1205c6a9b58282dbda30
Seong <kirakim96@hotmail.com>
2018-12-10 14:15:24 -0600

    Started on snackbar

--- client/src/containers/Page/index.js
+++ client/src/containers/Page/index.js
@@ -1,4 +1,7 @@
 import React, { Component, Fragment } from 'react'
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import { SnackbarProvider, withSnackbar } from 'notistack';
 import { CssBaseline, MuiThemeProvider } from '@material-ui/core'
 import { createMuiTheme } from '@material-ui/core/styles'
 
@@ -12,24 +15,50 @@ const theme = createMuiTheme({
 })
 
 class Page extends Component {
+  handleClick = () => {
+    this.props.enqueueSnackbar('I love snacks.');
+  }
+
+  handleClickVariant = (variant, message) => () => {
+    // variant could be success, error, warning or info
+    this.props.enqueueSnackbar(message, { variant });
+  }
+
   render () {
     return (
       <Fragment>
         <CssBaseline />
         <MuiThemeProvider theme={theme}>
-          {/* <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Sidebar />
-            <Browser />
-          </div> */}
-          {/* <Browser /> */}
           <Sidebar />
           <div style={{marginLeft: '210px'}}>
             <Browser />
           </div>
         </MuiThemeProvider>
+        
+        <React.Fragment>
+          <Button onClick={this.handleClick}>Show snackbar</Button>
+          <Button onClick={this.handleClickVariant('success','Success')}>Show Success snackbar</Button>
+          <Button onClick={this.handleClickVariant('warning','WARNING')}>Show Warning snackbar</Button>
+          <Button onClick={this.handleClickVariant('error','ERROR')}>Show Error snackbar</Button>
+          <Button onClick={this.handleClickVariant('info','Info')}>Show Info snackbar</Button>
+        </React.Fragment>
       </Fragment>
     )
   }
 }
 
-export default Page
+Page.propTypes = {
+  enqueueSnackbar: PropTypes.func.isRequired,
+};
+
+const MyPage = withSnackbar(Page);
+
+function IntegrationNotistack() {
+  return (
+    <SnackbarProvider maxSnack={3}>
+      <MyPage />
+    </SnackbarProvider>
+  );
+}
+
+export default IntegrationNotistack;
