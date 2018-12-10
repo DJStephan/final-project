2b29ac0a441f7b87de400f4317feadb9d1573ca4
Kalkazar <joecurrystodder@gmail.com>
2018-12-10 09:57:45 -0600

    finished implementing delete button

--- client/src/components/Delete/index.js
+++ client/src/components/Delete/index.js
@@ -20,48 +20,55 @@ class Delete extends Component {
     super()
     this.state = {
       open: false,
-      name: '[fileName/folderName]',
-      type: '[selected element]'
+      message: 'Please select a file or folder',
+      title: 'Nothing Selected'
     }
     this.textInput = React.createRef()
   }
 
   state = {
     open: false,
-    name: '[fileName/folderName]',
-    type: '[selected element]'
+    message: 'Please select a file or folder',
+    title: 'Nothing Selected'
   }
 
   handleOpen = () => {
-    // CLEAN THIS UP LATER
     if (!this.state.open) {
       this.setState({
-        // might consider removing this and adding it below
-        open: true // that way, things won't open until you select something
+        open: true
       })
       if (this.props.selectedFile) {
-        // CAN'T ACCESS STUFF IN ROOT FOLDER
         this.props.folders.filter(folder => {
           folder.files.filter(file => {
             if (file.id === this.props.selectedFile) {
               this.setState({
-                name: file.fileName,
-                type: 'File'
+                message: `Are you sure you want to delete "${file.fileName}"?`,
+                title: 'Delete File'
               })
             }
           })
         })
+        if (this.props.selectedFolder === 1) {
+          this.props.files.filter(file => {
+            if (file.id === this.props.selectedFile) {
+              this.setState({
+                message: `Are you sure you want to delete "${file.fileName}"?`,
+                title: 'Delete File'
+              })
+            }
+          })
+        }
       } else {
         this.props.folders.filter(folder => {
           if (folder.id === this.props.selectedFolder) {
             this.setState({
-              name: folder.folderName,
-              type: 'Folder'
+              message: `Are you sure you want to delete "${folder.folderName}"?`,
+              title: 'Delete Folder'
             })
           }
         })
-      } // maybe do this with ternary operators?
-    } // also maybe add something for when nothing is selected?
+      }
+    }
   }
 
   handleClose = () => {
@@ -76,6 +83,29 @@ class Delete extends Component {
   }
 
   render () {
+    const handleButtons = () => {
+      if (this.state.title === 'Nothing Selected') {
+        return (
+          <DialogActions>
+            <Button onClick={this.handleClose} color='primary'>
+              Ok
+            </Button>
+          </DialogActions>
+        )
+      } else {
+        return (
+          <DialogActions>
+            <Button onClick={this.trashOrDelete} color='primary'>
+              Yes
+            </Button>
+            <Button onClick={this.handleClose} color='primary'>
+              No
+            </Button>
+          </DialogActions>
+        )
+      }
+    }
+
     return (
       <ListItem button onClick={this.handleOpen}>
         {this.props.children}
@@ -88,22 +118,11 @@ class Delete extends Component {
           aria-describedby='alert-dialog-slide-description'
           scroll='paper'
         >
-          {/* Should we center everything? It might look better... */}
-          <DialogTitle>Delete {this.state.type}</DialogTitle>
+          <DialogTitle>{this.state.title}</DialogTitle>
           <DialogContent>
-            <DialogContentText>
-              Are you sure you want to delete "{this.state.name}"?
-            </DialogContentText>
+            <DialogContentText>{this.state.message}</DialogContentText>
           </DialogContent>
-          {/* Maybe we should re-order these buttons as well... */}
-          <DialogActions>
-            <Button onClick={this.trashOrDelete} color='primary'>
-              Yes
-            </Button>
-            <Button onClick={this.handleClose} color='primary'>
-              No
-            </Button>
-          </DialogActions>
+          {handleButtons()}
         </Dialog>
       </ListItem>
     )
@@ -111,6 +130,7 @@ class Delete extends Component {
 }
 
 const mapStateToProps = state => ({
+  files: state.files,
   folders: state.folders,
   selectedFile: state.selectedFile,
   selectedFolder: state.selectedFolder
@@ -119,11 +139,5 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   trashOrDeleteFileOrFolder: () => dispatch(trashOrDeleteFileOrFolder())
 })
-// const trashOrDeleteFileOrFolder = () => (dispatch, getState) => {
-// const moveFileOrFolder = (id, destinationId) => (dispatch, getState) => {
-// moveFileOrFolder: (id, destinationId, which) => moveFileOrFolder(id, destinationId, which)
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Delete)
+export default connect(mapStateToProps, mapDispatchToProps)(Delete)
